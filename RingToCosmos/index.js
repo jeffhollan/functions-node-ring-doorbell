7419af2d37c72a868614dfdc5be7ff69bb2a194b
Jeff Hollan <jehollan@microsoft.com>
2017-12-19 22:55:31 -0800

    taking ID from EventGrid if present

--- RingToCosmos/index.js
+++ RingToCosmos/index.js
@@ -4,7 +4,7 @@ module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     var ringData = req.body[0];
-    ringData['id'] = utils.generateUUID();
+    ringData['id'] = ringData['id'] || utils.generateUUID();
     context.log(ringData);
     context.bindings.ringDocument = JSON.stringify(ringData);
 
