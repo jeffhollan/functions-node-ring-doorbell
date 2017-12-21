ed2e66c8c288287b3ed92b2a5ffcf7d23b3f3964
Jeff Hollan <jehollan@microsoft.com>
2017-12-20 20:43:38 -0800

    not expecting an array

--- GetRecording/index.js
+++ GetRecording/index.js
@@ -2,7 +2,9 @@ const ring = require('../ring-api').ring_client;
 
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
-    ring.recording(req.body[0]['data']['id_str'], (e, recording) => {
+    // Not grabbing it from an array as expecting to get event from a Logic App and not
+    // directly from Azure Event Grid
+    ring.recording(req.body['data']['id_str'], (e, recording) => {
         context.log(`got recording link: ${recording}`);
         if(recording) {
             context.res = {
